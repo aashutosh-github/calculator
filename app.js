92fa18277200800942463bcc0f079ad29e0b3bd4
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2025-09-06 20:09:03 +0530

    Fix: wrote the entire logic again from scratch

--- app.js
+++ app.js
@@ -2,73 +2,24 @@ const numbersContainer = document.querySelector(".numbersContainer");
 const resultScreen = document.querySelector(".displayScreen");
 const resetButton = document.querySelector(".resetButton");
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+let expressionString = "";
 
-//initialising the initial state of the
-//calculator
-let numbers = {
-  firstNumber: 0,
-  operator: 0,
-  secondNumber: 0,
-};
-
-let sNum = parseFloat(numbers.secondNumber);
-let fNum = parseFloat(numbers.firstNumber);
+function reset() {
+  expressionString = "";
+  resultScreen.innerText = "";
+}
 
 //adding event listener to all the buttons
 //using event delegation
 numbersContainer.addEventListener("click", (event) => {
-  let clickedButton = event.target.innerHTML;
-  if (numbers.firstNumber === 0 && isNaN(clickedButton)) {
-    alert(`Please enter some numbers first!`);
-  }
-
-  if (!isNaN(clickedButton) && numbers.operator === 0) {
-    fNum = fNum * 10 + parseFloat(clickedButton);
-    numbers.firstNumber = fNum;
-  } else if (numbers.operator === 0) {
-    numbers.operator = clickedButton;
-  } else if (!isNaN(clickedButton)) {
-    sNum = sNum * 10 + parseFloat(clickedButton);
-    numbers.secondNumber = sNum;
-  } else if (numbers.secondNumber && clickedButton === "=") {
-    checkResult(clickedButton);
+  if (expressionString.length === 0 && event.target.innerText === "=") {
+    alert(`Please enter a valid mathematical expression!`);
+  } else if (event.target.innerText === "=") {
+    resultScreen.innerText = eval(expressionString);
+  } else {
+    expressionString += event.target.innerText;
+    resultScreen.innerText = expressionString;
   }
 });
 
-function checkResult(clickedButton) {
-  if (numbers.secondNumber && clickedButton === "=") {
-    let sNum = parseFloat(numbers.secondNumber);
-    let fNum = parseFloat(numbers.firstNumber);
-    switch (numbers.operator) {
-      case "+":
-        resultScreen.innerHTML = add(fNum, sNum);
-        break;
-      case "-":
-        resultScreen.innerHTML = subtract(fNum, sNum);
-        break;
-      case "x":
-        resultScreen.innerHTML = multiply(fNum, sNum);
-        break;
-      case "/":
-        resultScreen.innerHTML = divide(fNum, sNum);
-        break;
-      default:
-        console.log(
-          `Exiting the switch statement with no operation performed!`
-        );
-    }
-  }
-}
-
-//adding event listener to the clear button
-resetButton.addEventListener("click", () => {
-  resultScreen.innerHTML = "";
-  for (let key in numbers) {
-    numbers[key] = 0;
-  }
-  [sNum, fNum] = [0, 0];
-});
+resetButton.addEventListener("click", reset);
